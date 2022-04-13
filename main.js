1e5e359d182bcb633e3b90a929e61dd9af573e1f
Asenata Łuczak <asenata.luczak@bbraun.com>
2022-04-13 17:06:47 +0200

    adding newTask to unfinishedTasks array

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
-console.log('cos')
+// DEKLAROWANIE ZMIENNYCH
+// var nazwa1 = 'wartosc';
+// let nazwa1 = 'wartosc';
+// const nazwa2 = 'wartosc';
+
+// DOM - DOCUMENT OBJECT MODEL
+// DOM properties !== HTML attributes
+// console.log(element); -> element
+// console.dir(element); -> przedstawienie w DOM
+
+// DOM EVENT
+
+// TWORZENIE FUNKCJI
+// 1. function name() {}
+
+let addTaskForm = document.querySelector("form#addTaskForm");
+
+let unfinishedTasks = [];
+
+console.dir(addTaskForm);
+
+addTaskForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  if (addTaskForm.elements["title"].value) {
+    const newTask = {
+      title: addTaskForm.elements["title"].value,
+      description: addTaskForm.elements["description"].value,
+      id: Date.now(),
+    };
+    unfinishedTasks.push(newTask);
+    showUnfinishedTasks();
+    addTaskForm.reset();
+    console.log(unfinishedTasks);
+  }
+});
+
+function showUnfinishedTasks() {
+    
+}
